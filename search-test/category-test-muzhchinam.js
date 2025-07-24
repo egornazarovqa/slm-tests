985b2640659c1e9944314f31d361d80948962068
egor nazarov <egorvladimirovich1992@gmail.com>
2025-07-25 03:44:39 +0400

    Добавляю тесты категорий

---
+++ search-test/category-test-muzhchinam.js
@@ -0,0 +1,48 @@
+const { Builder, By, until, } = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+const assert = require("assert");
+
+//mocha describe block
+describe("muzhchinam test", function() {
+    //timeout
+    this.timeout(30000);
+    //headless
+    let driver;
+
+    before(async function() {
+        this.timeout(30000);
+        const options = new firefox.Options();
+        options.addArguments(
+            '--headless',
+            '--disable-gpu'
+        );
+        
+    after(async function() {
+        await driver.quit();
+    });
+
+    //открыть браузер
+    driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
+
+    });
+
+    //mocha it для muzhchinam
+    it("muzhchinam category test", async function() {
+
+        //открыть страницу категории
+        await driver.get('https://www.salonsecret.ru/muzhchinam');
+        
+        //принять куки
+        const cookiebtn = await driver.wait(until.elementLocated(By.xpath("//button[contains(text(), 'Принять все cookies')]")), 10000);
+        await driver.wait(until.elementIsVisible(cookiebtn), 5000);
+        await driver.wait(until.elementIsEnabled(cookiebtn), 5000);
+        await cookiebtn.click();
+
+        //проверить результат
+        const headerElement = await driver.wait(until.elementLocated(By.css('h1')), 10000);
+        const headerText = await headerElement.getText();
+        assert.strictEqual(headerText, "МУЖЧИНАМ");
+
+    });
+
+});
