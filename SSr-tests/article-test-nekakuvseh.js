600a35ee2006fc53ae9a56174b807f18e6166b56
egor nazarov <egorvladimirovich1992@gmail.com>
2025-07-25 04:03:53 +0400

    Выливаю тесты статей

---
+++ SSr-tests/article-test-nekakuvseh.js
@@ -0,0 +1,48 @@
+const { Builder, By, until, } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const assert = require("assert");
+
+//mocha describe block
+describe("ne kak u vseh test", function() {
+    //timeout
+    this.timeout(30000);
+    //headless
+    let driver;
+
+    before(async function() {
+        this.timeout(30000);
+        const options = new chrome.Options();
+        options.addArguments(
+            '--headless',
+            '--disable-gpu'
+        );
+        
+    after(async function() {
+        await driver.quit();
+    });
+
+    //открыть браузер
+    driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+
+    });
+
+    //mocha it для ne kak u vseh
+    it("Ne kak u vseh article test", async function() {
+
+        //открыть страницу статьи
+        await driver.get('https://www.salonsecret.ru/strizhki/trendy/asimmetrichnaya-chelka');
+        
+        //принять куки
+        const cookiebtn = await driver.wait(until.elementLocated(By.xpath("//button[contains(text(), 'Принять все cookies')]")), 10000);
+        await driver.wait(until.elementIsVisible(cookiebtn), 5000);
+        await driver.wait(until.elementIsEnabled(cookiebtn), 5000);
+        await cookiebtn.click();
+
+        //проверить результат
+        const headerElement = await driver.wait(until.elementLocated(By.css('h1')), 10000);
+        const headerText = await headerElement.getText();
+        assert.strictEqual(headerText, "НЕ КАК У ВСЕХ: АСИММЕТРИЧНАЯ ЧЕЛКА");
+
+    });
+
+});
